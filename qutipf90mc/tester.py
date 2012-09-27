d3e19ff42f02b9128a24219cca4ccdf3b91e29e3
Arne Grimsmo <arne.grimsmo@gmail.com>
2012-09-27 22:16:41 +1200

    commit for the night.

--- qutipf90mc/tester.py
+++ qutipf90mc/tester.py
@@ -1,22 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from qutip import *
+import qutip as qt
 import qutipf90mc as mcf90
 import time
 
 def test():
     gamma = 1
     neq = 2
-    psi0 = basis(neq,0)
+    psi0 = qt.basis(neq,0)
     #psi0 = Qobj([[1],[1]])
     #a = destroy(neq)
     #ad = a.dag()
     #H = ad*a
     #c_ops = [gamma*a]
     #e_ops = [ad*a]
-    H = sigmax()
-    c_ops = [sqrt(gamma)*sigmax()]
-    e_ops = [sigmam()*sigmap(),sigmap()*sigmam()]
+    H = qt.sigmax()
+    c_ops = [np.sqrt(gamma)*qt.sigmax()]
+    e_ops = [qt.sigmam()*qt.sigmap(),qt.sigmap()*qt.sigmam()]
     #e_ops = [sigmam()*sigmap()]
 
     # Times
@@ -25,10 +25,10 @@ def test():
     nstep = int(T/dt)
     tlist = np.linspace(0,T,nstep)
 
-    ntraj=10000
+    ntraj=1000
 
     # set options
-    opts = Odeoptions()
+    opts = qt.Odeoptions()
     #opts.num_cpus=1
     #opts.gui=False
     #opts.max_step=1000
@@ -37,15 +37,15 @@ def test():
 
     start_time = time.time()
     sol_f90 = mcf90.mcsolve_f90(H,psi0,tlist,c_ops,e_ops,ntraj=ntraj,options=opts)
-    print "solutiton took", time.time()-start_time, "s"
+    print "mcsolve_f90 solutiton took", time.time()-start_time, "s"
 
     start_time = time.time()
-    sol_me = mesolve(H,psi0,tlist,c_ops,e_ops,options=opts)
-    print "solutiton took", time.time()-start_time, "s"
+    sol_me = qt.mesolve(H,psi0,tlist,c_ops,e_ops,options=opts)
+    print "mesolve solutiton took", time.time()-start_time, "s"
 
     start_time = time.time()
-    sol_mc = mcsolve(H,psi0,tlist,c_ops,e_ops,ntraj=ntraj,options=opts)
-    print "solutiton took", time.time()-start_time, "s"
+    sol_mc = qt.mcsolve(H,psi0,tlist,c_ops,e_ops,ntraj=ntraj,options=opts)
+    print "mcsolve solutiton took", time.time()-start_time, "s"
 
     plt.figure()
     for i in range(len(e_ops)):
@@ -55,3 +55,5 @@ def test():
 
     return sol_f90
 
+if __name__ == '__main__':
+    test()
