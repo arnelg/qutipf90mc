a17b15ee4804003b9998841b57e6d7ac91940a49
Arne Grimsmo <arne.grimsmo@gmail.com>
2017-04-10 19:48:52 -0400

    Make qutip 4.2.0 compatible

--- qutipf90mc/examples/__init__.py
+++
@@ -1,6 +0,0 @@
-import qutipf90mc.examples.ex_24
-import qutipf90mc.examples.ex_30
-import qutipf90mc.examples.ex_31
-#import qutipf90mc.examples.ex_32
-import qutipf90mc.examples.ex_33
-import qutipf90mc.examples.ex_34
