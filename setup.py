e60f8d1f635426b8feb54af4dcf2882c31ff4907
Arne Grimsmo <arne.grimsmo@gmail.com>
2017-04-11 14:16:27 -0400

    Revert to old setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+"""
+qutipf90mc doc
+"""
 
-setup(
-        name="qutipf90mc",
-        version="0.2",
-        packages=find_packages(),
-        author="Arne L. Grimsmo",
-        license="MIT",
-        install_requires=["numpy", "scipy"],
-)
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+    config.add_subpackage('qutipf90mc')
+    config.set_options(ignore_setup_xxx_py=True,
+                       assume_default_configuration=True,
+                       delegate_options_to_subpackages=True,
+                       quiet=True)
+    return config
+
+
+def setup_package():
+    from numpy.distutils.core import setup
+    try:
+        setup(
+            name='qutipf90mc',
+            version='0.2',
+            maintainer="Arne L. Grimsmo",
+            maintainer_email="arne.grimsmo@gmail.com",
+            configuration=configuration)
+    finally:
+        pass
+    return
+
+
+if __name__ == '__main__':
+    setup_package()
